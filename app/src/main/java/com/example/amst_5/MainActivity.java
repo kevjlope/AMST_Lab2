aec010764189c803d6feaa394991e8e1bb1243f8
kevjlope <kevinjeff04@gmail.com>
2020-06-24 22:47:49 -0500

    Provando

--- app/src/main/java/com/example/amst_5/MainActivity.java
+++ app/src/main/java/com/example/amst_5/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.amst_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarVideo(View view){
+
+
     }
 }
